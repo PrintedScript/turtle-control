f80d4c50fe98c8c54d047abb2aa5ee5635d8228b
PrintedScript <64908610+PrintedScript@users.noreply.github.com>
2022-11-19 20:31:36 +0800

    removed legacy code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import { Server } from 'ws';
-// import { connect } from 'ngrok';
-//import { App, launch } from 'carlo';
-//import { resolve } from 'path';
 import { Turtle } from './turtle';
 import World from './world';
 import Queue from 'p-queue';
@@ -10,7 +7,6 @@ const wss = new Server({ port: 5757 });
 const wss_frontend = new Server({ port: 5758 });
 
 console.log('Started Turtle Control WS Server on port 5757 and port 5758');
-
 //let app: App;
 let turtles: { [id: number]: Turtle } = {};
 
@@ -26,26 +22,6 @@ async function SendFrontendData( type: string, data: any ) {
 }
 
 (async () => {
-	// const url = await connect(5757);
-	// console.log(url);
-	//app = await launch();
-	//app.on('exit', () => process.exit());
-	//app.serveFolder(resolve(process.cwd(), "frontend/out"));
-	// app.load('http://localhost:3000');
-
-	//app.exposeFunction('exec', async (index: number, func: string, ...args: any[]) => {
-		//if (typeof index === 'string') {
-			//[index, func, ...args] = JSON.parse(index).args;
-		//}
-		//return await queue.add(() => ((turtles[index] as any)[func])(...args));
-	//});
-
-	//app.exposeFunction('refreshData', async () => {
-		//await app.evaluate(`if (window.setWorld) window.setWorld(${JSON.stringify(world.getAllBlocks())})`);
-		//await app.evaluate(`if (window.setTurtles) window.setTurtles(${serializeTurtles()})`);
-	//})
-
-	//await app.load('http://localhost:3000');
 	world.on('update', async (world) => {
 		await SendFrontendData( 'world', world );
 	});
@@ -73,15 +49,11 @@ wss.on('connection', async function connection(ws,r) {
 		let turtle = new Turtle(ws, world);
 		turtle.on('init', async () => {
 			turtles[turtle.id] = turtle;
-			//turtle.on('update', () => app.evaluate(`if (window.setTurtles) window.setTurtles(${serializeTurtles()})`));
 			turtle.on('update', async () => SendFrontendData( 'turtles', await serializeTurtles() ) );
-			//await app.evaluate(`if (window.setTurtles) window.setTurtles(${serializeTurtles()})`)
-			//await app.evaluate(`if (window.setWorld) window.setWorld(${JSON.stringify(world.getAllBlocks())})`);
 			await SendFrontendData( 'turtles', await serializeTurtles() );
 			await SendFrontendData( 'world', world.getAllBlocks() );
 			ws.on('close', async () => {
 				delete turtles[turtle.id];
-				//await app.evaluate(`if (window.setTurtles) window.setTurtles(${serializeTurtles()})`)
 				await SendFrontendData( 'turtles', await serializeTurtles() );
 			});
 		});
@@ -111,7 +83,7 @@ wss_frontend.on('connection', async function connection(ws, r) {
 					ws.send( JSON.stringify( { type: 'exec_response', data: ExecResponse, nonce: MessageData.nonce } ) );
 			}
 		} catch( e ) {
-			console.log( e );
+			console.log(`Error while processing frontend message: ${e}`);
 		}
 	});
 });
